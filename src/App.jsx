44db2a57582f95c308a6dcc2617e61b1b8cd98d5
Mark <mark.matsue@gmail.com>
2024-01-30 10:03:05 +0100

    fix params object issue.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
 import Home from "./pages/Home";
 import GameDetail from "./pages/GameDetail";
 import { useState } from "react";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
   return (
     <BrowserRouter>
-      <Navbar onSearch={(term) => setSearchTerm(term)} />
-
       <main>
         <Routes>
-          <Route path="/" element={<Home searchTerm={searchTerm} />} />
+          <Route path="/" element={<Home />} />
           <Route path="/game/:id" element={<GameDetail />} />
         </Routes>
       </main>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,60 +4,63 @@ import Genres from "../components/Genres";
 import Platforms from "../components/Platforms";
 import GamesSort from "../components/GamesSort";
 import Games from "../components/Games";
+import Navbar from "./Navbar";
 
-function Home({ searchTerm }) {
-  //for css class
+function Home() {
   const [gameQuery, setGameQuery] = useState({
     selectedGenreId: null,
     selectedPlatformId: null,
-    searchTerm,
+    searchTerm: null,
   });
-
-  console.log(gameQuery.searchTerm);
   const [errors, setErrors] = useState([]);
 
   const handleGenreSelect = (genre_id) =>
     setGameQuery({ ...gameQuery, selectedGenreId: genre_id });
+  const handleSearch = (term) =>
+    setGameQuery({ ...gameQuery, searchTerm: term });
   const handlePlatformSelect = (platform_id) =>
     setGameQuery({ ...gameQuery, selectedPlatformId: platform_id });
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <Genres
-          onGenreSelect={handleGenreSelect}
-          onError={(m) => setErrors([...errors, m])}
-        />
-        <div className="col">
-          <div className="row">
-            <h1 className="fw-bolder my-3">Games</h1>
-            <div className="col-12">
-              <div className="row">
-                <div className="col-3">
-                  <Platforms
-                    onError={(m) => setErrors([...errors, m])}
-                    onPlatformSelect={handlePlatformSelect}
-                  />
-                </div>
-                <div className="col-3">
-                  <GamesSort />
+    <>
+      <Navbar onSearch={handleSearch} />
+      <div className="container-fluid">
+        <div className="row">
+          <Genres
+            onGenreSelect={handleGenreSelect}
+            onError={(m) => setErrors([...errors, m])}
+          />
+          <div className="col">
+            <div className="row">
+              <h1 className="fw-bolder my-3">Games</h1>
+              <div className="col-12">
+                <div className="row">
+                  <div className="col-3">
+                    <Platforms
+                      onError={(m) => setErrors([...errors, m])}
+                      onPlatformSelect={handlePlatformSelect}
+                    />
+                  </div>
+                  <div className="col-3">
+                    <GamesSort />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-12">
-              {errors[0] && (
-                <div className="alert alert-danger w-50 m-4">{errors[0]}</div>
-              )}
+              <div className="col-12">
+                {errors[0] && (
+                  <div className="alert alert-danger w-50 m-4">{errors[0]}</div>
+                )}
 
-              <Games
-                gameQuery={gameQuery}
-                onError={(m) => setErrors([...errors, m])}
-              />
+                <Games
+                  gameQuery={gameQuery}
+                  onError={(m) => setErrors([...errors, m])}
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
